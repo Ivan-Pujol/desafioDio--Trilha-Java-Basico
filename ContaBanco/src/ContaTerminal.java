14a02ed2ec38c582a4c64820cb9bfc43ac509508
Ivan-Pujol <ivan.ivanovich@hotmail.com>
2023-06-20 20:28:24 -0300

    Criação das variáveis, scanner e mensagem final

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int contaNumero;
+        String agencia, nomeCliente;
+        double saldo;
+        Random gerador = new Random();
+
+        System.out.println("Digite o Nome do Cliente");
+        nomeCliente = scanner.nextLine();
+        System.out.println("Digite a agencia");
+        agencia = scanner.nextLine();
+        contaNumero = gerador.nextInt(1,9999);
+        System.out.println("Digite o valor depositado na criação da conta");
+        saldo = scanner.nextDouble();
+
+        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+",\n conta "+contaNumero+" e seu saldo "+saldo+" já está disponível para saque");
+
+
+    }
+}
